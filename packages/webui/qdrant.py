cd067125d2b9a68f1c857b21f7b5098c7cc1c682
John Miller <jbmiller10@gmail.com>
2025-11-28 14:07:37 -0600

    fix circular import

--- packages/webui/qdrant.py
+++ packages/webui/qdrant.py
@@ -7,7 +7,7 @@ from threading import Lock
 
 from qdrant_client import QdrantClient
 
-from packages.webui.utils.retry import exponential_backoff_retry
+from webui.utils.retry import exponential_backoff_retry
 from shared.config import settings
 from shared.managers.qdrant_manager import QdrantManager
 
