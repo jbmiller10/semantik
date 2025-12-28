3462410eb1135af4e28701a616b253edfe4d844b
John Miller <jbmiller10@gmail.com>
2025-12-28 13:55:44 -0600

    fix: use type-only import for InternalAxiosRequestConfig
    Required by verbatimModuleSyntax TypeScript option.

--- apps/webui-react/src/services/api/v2/client.ts
+++ apps/webui-react/src/services/api/v2/client.ts
@@ -1,4 +1,5 @@
-import axios, { AxiosError, InternalAxiosRequestConfig } from 'axios';
+import axios, { AxiosError } from 'axios';
+import type { InternalAxiosRequestConfig } from 'axios';
 import { useAuthStore } from '../../../stores/authStore';
 import { navigateTo } from '../../navigation';
 import { getApiBaseUrl } from '../baseUrl';
