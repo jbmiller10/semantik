fec93f51dd957a99b1d3f714d8d3c748b09fb481
John Miller <jbmiller10@gmail.com>
2026-01-25 16:44:15 -0600

    feat(pipeline): export configuration panel components

--- apps/webui-react/src/components/pipeline/index.ts
+++ apps/webui-react/src/components/pipeline/index.ts
@@ -2,4 +2,8 @@
 export { PipelineVisualization } from './PipelineVisualization';
 export { PipelineNodeComponent } from './PipelineNode';
 export { PipelineEdgeComponent } from './PipelineEdge';
+export { ConfigurationPanel } from './ConfigurationPanel';
+export { NodeConfigEditor } from './NodeConfigEditor';
+export { EdgePredicateEditor } from './EdgePredicateEditor';
+export { SourceAnalysisSummary } from './SourceAnalysisSummary';
 export type { PipelineVisualizationProps } from '@/types/pipeline';
